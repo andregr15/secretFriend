76168fc5ba29969d6be84ba1756a22073bea7202
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-06-22 07:15:16 -0300

    Ajusting mailer test

--- spec/mailers/campaign_mailer_spec.rb
+++ spec/mailers/campaign_mailer_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe CampaignMailer, type: :mailer do
 
     it "renders the headers" do
       expect(@mail.subject).to eq("Nosso Amigo Secreto: #{campaign.title}")
-      expect(@mail.to).to eql([@member.email])
+      expect(@mail.to).to eq([@member.email])
     end
 
     it "body have member name" do
