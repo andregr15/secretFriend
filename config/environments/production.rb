c34300f39119b370a683fc04f7f20e62815d59fc
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-06-27 23:38:03 -0300

    Adding docker-compose-prod.yml, doing nginx settings and ajusting app for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  #config.action_mailer.default_url_options = { host: request.host_with_port }
+  config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.raise_delivery_errors = true
