e5d8edaf4cc8a2e399a03bc0928dd4f433294914
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-06-28 00:17:11 -0300

    Updating action_mailer.default_url_options to the ip on digital ocean

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
+  config.action_mailer.default_url_options = { host: '45.55.37.138' }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.raise_delivery_errors = true
